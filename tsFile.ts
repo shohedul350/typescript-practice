3893c3915a6b2b2ec87105cebd33d3d13458571e
shohedul350 <shohedul350@gmail.com>
2020-12-20 22:57:31 +0600

    array and object done

--- tsFile.ts
+++ tsFile.ts
@@ -1,12 +1,39 @@
-const character = 'Emon...';
-
-console.log(character);
-
-const inputs = document.querySelectorAll('input');
-
-console.log(inputs);
+// array
+var names = ['shohedul', 'naser', 'shanto'];
+names.push('emran');
+// names.push(3);  we can't do this
+//  names[1] = 3;   we can't do this
 
 
-inputs.forEach(input => { 
-  console.log(input)
-});
+let numbers = [10, 20, 12, 15];
+
+numbers.push(25);
+// numbers.push('shaun'); we can't do this
+// nu
+
+
+
+let mixed = ['emon', 4, 'shanto', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// object
+
+let person = {
+  name: 'shohedul',
+  color: 'blue',
+  age: 30
+};
+person.age = 40;
+person.name = 'ryu';
+// person.age = '30';  we can't do this
+// person.skills = ['javascript,python']  we can't do this
+
+
+person = {
+  name: 'yoshi',
+  color: 'orange',
+  age: 40,
+  // skills: ['running'], we can't do this
+};
